39eb294a932fa15e9b0e81e30d910fbf60f7ff7f
AmaliaArstamyan <Rokinrol13.>
2026-03-12 23:13:02 +0400

    update DB

--- config/settings.py
+++ config/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smart_tourist_map',
+        'USER': 'postgres',
+        'PASSWORD': 'test1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
