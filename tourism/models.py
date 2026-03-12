74ad982c364790db8dfea68931b12d6fe5b5086e
AmaliaArstamyan <Rokinrol13.>
2026-03-12 23:23:25 +0400

    check

--- tourism/models.py
+++ tourism/models.py
@@ -32,6 +32,7 @@ class Place(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    
     def __str__(self):
         return self.name
     
